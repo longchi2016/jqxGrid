04ab1d1dc83df03d365638f384d623b49890fff5
Aioub Amini <amini.a@DBI.COM>
2016-07-12 15:50:05 -0700

    https://github.com/LinkedsoftCo/jqxGrid.git

---
+++ lswidgets/jqx-grid.js
@@ -0,0 +1,16 @@
+/*
+ * jQuery table plugin 1.0
+ *
+ * Copyright (c) 2016 Aioub Amini , ayyub.amini@gmail.com
+ *
+ * MIT licensed   
+ * http://www.linkedsoft.ir/
+
+ * Date: Tue July 12 2016
+ */
+//
+
+$.fn.jqxGrid = Function (options){
+    $("#mygrid").html("<div class='table'><tr><td>1</td><td>2</td><td>3</td></tr> </div>");
+
+};
