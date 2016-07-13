b01179f766123b9aa29dfae3f0d5e44fc0066978
Aioub Amini <amini.a@DBI.COM>
2016-07-13 15:53:44 -0700

    update

--- lswidgets/jqx-grid.js
+++ lswidgets/jqx-grid.js
@@ -11,10 +11,30 @@
 //
 
 (function ($) {
-   
-    $.fn.jqxGrid = function() {
-        
-        this.append("<table>");
-        
+
+    var methods = {
+        init: function (options) {
+            //alert("Hi this a new plugin.");
+            var tableDiv = $("<table />").addClass("table table-hover table-responsive").append("<thead><tr><th>Code</th><th>Name</th><th>Family</th></tr></thead>");
+            tableDiv.append("<tbody><tr><td>1</td><td>Ali</td><td>Ahmadi</td></tr><tr><td>2</td><td>nasim</td><td>maleki</td></tr><tr><td>3</td><td>Aioub</td><td>Amini</td></tr></tbody>");
+
+            this.append(tableDiv);
+            return;
+        }
+    };
+
+    $.fn.jqxGrid = function (method) {
+
+        // Method calling logic
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.jqxGrid');
+        }
+
+    };
 
 } (jQuery));
+
